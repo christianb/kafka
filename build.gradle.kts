f85d5c5cbce52d43e1b26b0978053361c8dd5bf2
Christian Bunk <christian.bunk@nordsec.com>
2025-04-24 13:15:05 +0200

    init project

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "2.1.20"
+}
+
+group = "kafka.course"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(23)
+}

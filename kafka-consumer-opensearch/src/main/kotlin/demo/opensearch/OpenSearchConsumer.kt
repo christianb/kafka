dfe7d4a9bf285eec18058d2eabb6741c1423c2c4
Christian Bunk <christian.bunk@nordsec.com>
2025-04-25 18:04:24 +0200

    create Wikimedia Index in OpenSearch

--- kafka-consumer-opensearch/src/main/kotlin/demo/opensearch/OpenSearchConsumer.kt
+++ kafka-consumer-opensearch/src/main/kotlin/demo/opensearch/OpenSearchConsumer.kt
@@ -1,11 +1,29 @@
 package demo.opensearch
 
-import config.KafkaFactory
-import org.apache.http.client.methods.HttpPost
-import org.opensearch.client.RestClient
-import org.opensearch.client.RestHighLevelClient
-import java.net.URI
+import config.log
+import org.opensearch.client.RequestOptions.DEFAULT
+import org.opensearch.client.indices.CreateIndexRequest
+import org.opensearch.client.indices.GetIndexRequest
+import java.io.IOException
+
+private const val WIKIMEDIA_INDEX = "wikimedia"
 
 fun main() {
+    val client = OpenSearch.client
+
+    val indicesClient = client.indices()
+
+    try {
+        val exists = indicesClient.exists(GetIndexRequest(WIKIMEDIA_INDEX), /* options = */ DEFAULT)
+        if (!exists) {
+            indicesClient.create(CreateIndexRequest(WIKIMEDIA_INDEX), /* options = */ DEFAULT)
+            log.info("Wikimedia index created")
+        } else log.info("Wikimedia index already exists")
+
+    } catch (e: IOException) {
+        log.error("error", e)
+    } finally {
+        client.close()
+    }
 }
 
